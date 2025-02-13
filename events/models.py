fbfec5f1febd6e6783c2d34bad9ce59e8673beda
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-02-13 20:32:02 +0000

    RSVP Model added

--- events/models.py
+++ events/models.py
@@ -22,5 +22,25 @@ class Event(models.Model):
     end_time = models.DateTimeField()
     max_capacity = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
-    host = models.ForeignKey(USER, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+
+    def __str__(self):
+        return self.title
+    
+
+class RSVP(models.Model):
+    STATUS_CHOICES = [
+        ('rsvp', 'RSVP')
+        ('going', 'Going'),
+        ('cancelled', 'Cancelled'),
+        ('waitlist', 'Waitlist'),
+    ]
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='rsvp')
+    timestamp = models.DateTimeField(auto_now=True)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='rsvps')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rsvps')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.event.title} ({self.status})"
