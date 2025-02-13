819007c417590130d04ca5db01309ac77694f6a0
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-02-13 21:07:24 +0000

    Added user import and fixed issue with empty urls.py in events app

--- events/models.py
+++ events/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Event(models.Model):
     CATEGORY_CHOICES = [
@@ -31,7 +34,7 @@ class Event(models.Model):
 
 class RSVP(models.Model):
     STATUS_CHOICES = [
-        ('rsvp', 'RSVP')
+        ('rsvp', 'RSVP'),
         ('going', 'Going'),
         ('cancelled', 'Cancelled'),
         ('waitlist', 'Waitlist'),
