3d6f33ed4e00abac146f0a2903229e3dc04cb1d2
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-02-13 20:08:58 +0000

    Added template file structure for events and updated Event Model and added it to admin file

--- events/models.py
+++ events/models.py
@@ -1,13 +1,26 @@
 from django.db import models
 
-# class Event(models.Model):
-#     title = models.CharField(max_length=200)
-#     description = models.TextField()
-#     date = models.DateTimeField()
-#     location_name = models.CharField(max_length=200)
-#     location_address = models.CharField(max_length=200)
-#     location_city = models.CharField(max_length=50)
-#     max_capacity = models.PositiveIntegerField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     host = models.ForeignKey(User, on_delete=models.CASCADE)
-#     category = models.CharField(max_length=50)
+class Event(models.Model):
+    CATEGORY_CHOICES = [
+        ('art', 'Art'),
+        ('games', 'Games'),
+        ('music', 'Music'),
+        ('sports', 'Sports'),
+        ('social', 'Social'),
+        ('adventure', 'Adventure'),
+        ('education', 'Education'),
+        ('professional', 'Professional'),
+        ('other', 'Other'),
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location_address = models.CharField(max_length=200)
+    location_city = models.CharField(max_length=50)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    max_capacity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    host = models.ForeignKey(USER, on_delete=models.CASCADE)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
--- events/views.py
+++ events/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
-
-def home(request):
-    return HttpResponse('<h1>Eventure Home</h1>')
-
-def about(request):
-    return HttpResponse('<h1>Eventure-About</h1>')
