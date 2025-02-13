4c84c03f4f938b59d81efbc26e92e5b2645dd9b2
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-02-13 19:34:42 +0000

    Added routes

--- events/models.py
+++ events/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-class Event(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    date = models.DateTimeField()
-    location_name = models.CharField(max_length=200)
-    location_address = models.CharField(max_length=200)
-    location_city = models.CharField(max_length=50)
-    max_capacity = models.PositiveIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    host = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.CharField(max_length=50)
+# class Event(models.Model):
+#     title = models.CharField(max_length=200)
+#     description = models.TextField()
+#     date = models.DateTimeField()
+#     location_name = models.CharField(max_length=200)
+#     location_address = models.CharField(max_length=200)
+#     location_city = models.CharField(max_length=50)
+#     max_capacity = models.PositiveIntegerField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     host = models.ForeignKey(User, on_delete=models.CASCADE)
+#     category = models.CharField(max_length=50)
