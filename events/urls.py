b0b26ba1b9f5a0853758f5bb14d868a5a17c4936
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-02-14 18:33:53 +0000

    Added event_list to views, urls and test code to event_list html

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+app_name = 'events'
 
+urlpatterns = [
+    path('', views.event_list, name='event_list')
 ]
--- events/views.py
+++ events/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Event
+from .forms import EventForm
 
 # Create your views here.
+
+def event_list(request):
+    events = Event.objects.all()
+    location = request.GET.get('location_city')
+    category = request.GET.get('category')
+
+    if location:
+        events = events.filter(location_city__icontains=location)
+
+    if category:
+        events = events.filter(category=category)
+    
+    return render(request, 'events/event_list.html', {'events': events})
+
