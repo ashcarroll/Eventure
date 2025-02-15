76cf8b2668eb2a3b2e7914639c401883d3135031
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-02-15 21:17:01 +0000

    Added ability to update events

--- events/urls.py
+++ events/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ( EventListView, EventDetailView, EventCreateView )
+from .views import ( EventListView, EventDetailView, EventCreateView, EventUpdateView )
 from . import views
 
 app_name = 'events'
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', EventListView.as_view(), name = "event_list"),
     path('event/<int:pk>', EventDetailView.as_view(), name='event_detail'),
     path('event/new/', EventCreateView.as_view(), name='event_create'),
+    path('event/<int:pk>/update/', EventUpdateView.as_view(), name='event_update'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Event
 from.forms import EventForm
-from django.views.generic import ( ListView, DetailView, CreateView )
+from django.views.generic import ( ListView, DetailView, CreateView, UpdateView )
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.datetime_safe import datetime
@@ -24,6 +24,23 @@ class EventCreateView(LoginRequiredMixin, CreateView):
     model = Event
     form_class = EventForm
     login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+
+        start_date = form.cleaned_data['start_date']
+        start_time = form.cleaned_data['start_time_input']
+        end_date = form.cleaned_data['end_date']
+        end_time = form.cleaned_data['end_time_input']
+
+        form.instance.start_time = datetime.combine(start_date, start_time)
+        form.instance.end_time = datetime.combine(end_date, end_time)
+
+        return super().form_valid(form)
+
+class EventUpdateView(LoginRequiredMixin, UpdateView):
+    model = Event
+    form_class = EventForm
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
