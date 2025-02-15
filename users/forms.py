c429f3fc2fb839fd320a6252ad621619dc322d68
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-02-15 13:16:50 +0000

    Added ability to update profile details

--- users/forms.py
+++ users/forms.py
@@ -1,21 +1,24 @@
 from django import forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
-
-User = get_user_model()
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_method = 'POST'
-        self.helper.add_input(Submit('submit', 'Sign Up'))
-
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from django.contrib.auth.models import User
+from PIL import Image
+import os
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,3 +9,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if os.path.exists(self.image.path):
+            with Image.open(self.image.path) as img:
+                if img.height > 100 or img.width > 100:
+                    output_size = (100, 100)
+                    img.thumbnail(output_size)
+                    img.save(self.image.path)
