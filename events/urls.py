aa6a264d4094aa0f8f72b16e40a08553ac93a875
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-02-15 13:54:43 +0000

    Event detail view added and preliminary html

--- events/urls.py
+++ events/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import EventListView
+from .views import EventListView, EventDetailView
 from . import views
 
 app_name = 'events'
 
 urlpatterns = [
     path('', EventListView.as_view(), name = "event_list"),
-    # path('event/<int:pk>/', views.event_detail, name='event_detail')
+    path('event/<int:pk>', EventDetailView.as_view(), name='event_detail'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Event
-from .forms import EventForm
 from django.views.generic import ListView, DetailView
 
 from django.contrib.auth import get_user_model
