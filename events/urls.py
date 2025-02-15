f120ce3d7656b891ce077c51291c974f2c0df274
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-02-15 13:42:28 +0000

    Changed approach for event creation / updating etc
    Added List View class

--- events/urls.py
+++ events/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from .views import EventListView
 from . import views
 
 app_name = 'events'
 
 urlpatterns = [
-    path('', views.event_list, name='event_list'),
-    path('event/<int:pk>/', views.event_detail, name='event_detail')
+    path('', EventListView.as_view(), name = "event_list"),
+    # path('event/<int:pk>/', views.event_detail, name='event_detail')
 ]
