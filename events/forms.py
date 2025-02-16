519c382adf95b25658e3d5a38bc7267657d4808b
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-02-16 16:26:37 +0000

    Updated import in events forms as was using an old django.utils.datetime_safe not compatible with new Django

--- events/forms.py
+++ events/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.utils.datetime_safe import datetime
+from datetime import datetime
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Row, Column
 from .models import Event
