7baf51cc435cc3fa7c506b995af80be82f4a732b
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-02-16 18:28:11 +0000

    Trying to fix the giatn default image, new image id added to Profile class

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = CloudinaryField('profile_image', default='default_onbmsq', folder='profile_pics', transformation={'width': 100, 'height': 100, 'crop': 'crop','gravity': 'face'}) 
+    image = CloudinaryField('profile_image', default='default_gjxs0u', folder='profile_pics', transformation=[{'width': 100, 'height': 100, 'crop': 'crop','gravity': 'face'},{'quality': 'auto'}]) 
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
