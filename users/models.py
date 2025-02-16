98f71de243e4c41a0e5c3f14d95451886ae496de
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-02-16 17:32:42 +0000

    Made changes so default image is comng frm cloudinary and updated models

--- users/models.py
+++ users/models.py
@@ -1,21 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
-import os
+from cloudinary.models import CloudinaryField
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.png', upload_to='profile_pics')
+    image = CloudinaryField('profile_image', default='default_onbmsq', folder='profile_pics') 
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        if os.path.exists(self.image.path):
-            with Image.open(self.image.path) as img:
-                if img.height > 100 or img.width > 100:
-                    output_size = (100, 100)
-                    img.thumbnail(output_size)
-                    img.save(self.image.path)
